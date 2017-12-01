7f28f91e02023b4dcc45e43c582ef6392a70d742
LZHD <lzhdfxq@163.com>
2017-12-01 17:00:40 +0800

    格式化

--- res/js/app.js
+++ res/js/app.js
@@ -1,4 +1,1 @@
-$('#share-1').share();
-$('#share-2').share({sites: ['qzone', 'qq', 'weibo','wechat']});
-$('#share-3').share();
-$('#share-4').share();
+$('#share-2').share({sites: ['qzone', 'qq', 'weibo']});
